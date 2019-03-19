88f993a9193c0e9353bccd982c5da6f38ef07629
Project-neuron <elvis.a.w.okumu@gmail.com>
2019-03-18 21:21:12 -0400

    created the team and roster classes

--- src/main/java/ithaca/edu/footballTeam/footballApp/Roster.java
+++ src/main/java/ithaca/edu/footballTeam/footballApp/Roster.java
@@ -1,4 +1,15 @@
 package ithaca.edu.footballTeam.footballApp;
 
 public class Roster {
+    private boolean canPlay;
+    //private List<Player> Players = new ArrayList();
+
+    public Roster(boolean canPlay){
+        this.canPlay = canPlay;
+
+    }
+
+    public boolean isEligable(){
+        return this.canPlay;
+    }
 }
--- src/main/java/ithaca/edu/footballTeam/footballApp/Team.java
+++ src/main/java/ithaca/edu/footballTeam/footballApp/Team.java
@@ -1,4 +1,34 @@
 package ithaca.edu.footballTeam.footballApp;
 
+
+
+
 public class Team {
+
+    private Roster activeRoster;
+    private Roster benchRoster;
+
+
+    public Team(Roster activeRoster){
+        if(activeRoster.isEligable() == false) {
+            throw new IllegalArgumentException("Entered team is not eligable to play");
+        }
+        else{
+            this.activeRoster = activeRoster;
+        }
+    }
+
+    public void setActiveRoster(Roster activeRoster){
+        this.activeRoster = activeRoster;
+    }
+
+    public void setBenchRoster(Roster benchRoster){
+        this.benchRoster = benchRoster;
+    }
+
+    public boolean isTeamElgable(){
+        return activeRoster.isEligable();
+    }
+
+
 }
