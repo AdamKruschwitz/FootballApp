9d20d24baa756c88d8d84d8600293c4dceff947f
Project-neuron <elvis.a.w.okumu@gmail.com>
2019-03-20 17:24:22 -0400

    Reading and writing IO simplification

--- src/main/java/ithaca/edu/footballTeam/footballApp/ReadingAndWritingIO.java
+++ src/main/java/ithaca/edu/footballTeam/footballApp/ReadingAndWritingIO.java
@@ -1,6 +1,7 @@
 package ithaca.edu.footballTeam.footballApp;
 
 import java.io.*;
+import java.util.Scanner;
 
 
 public class ReadingAndWritingIO {
@@ -13,43 +14,16 @@ public class ReadingAndWritingIO {
      */
 
 
-        public static void readLineByLine(BufferedReader br) throws IOException {
-            int count = 1;
-            String line = br.readLine();
-            while (line != null){
-                line = br.readLine();
 
-            }
-        }
+    public String getUserInput() {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String input = myObj.nextLine();  // Read user input
+        return input;
+    }
 
-        public static   void readFromFile(String fileName) throws IOException{
-            FileReader fr = new FileReader(fileName);
-            BufferedReader br = new BufferedReader(fr);
-            readLineByLine(br);
-            br.close();
-        }
-
-        public static void readFromConsole(String fileName) throws IOException{
-            InputStreamReader isr = new InputStreamReader(System.in);
-            BufferedReader br = new BufferedReader(isr);
-            readLineByLine(br);
-
-        }
-
-        public static void writeLineByLine(PrintStream ps, String input) throws Exception{
-            ps.println(input);
-        }
-
-        public static void writeToFile(String fileName, String input) throws Exception{
-            PrintStream ps = new PrintStream(new FileOutputStream(fileName));
-            writeLineByLine(ps, input);
-            ps.close();
-        }
-
-        public static void writeToConsole(String input) throws Exception{
-            writeLineByLine(System.out, input);
-
-        }
+    public void printToConsole(String input){
+        System.out.println(input);
+    }
 
 
 }
