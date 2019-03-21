3d15d72af36b4843a99f1fe67766abaf70ff83fa
esteele104 <esteele@ithaca.edu>
2019-03-20 23:18:03 -0400

    updated tests, removed registerTeam class and added teamOwner and Commissioner classes

---
+++ src/main/java/edu/ithaca/FootballApp/Commissioner.java
@@ -0,0 +1,32 @@
+package edu.ithaca.FootballApp;
+
+import java.util.Map;
+
+public class Commissioner {
+    private String name;
+
+    public Commissioner(String name){
+        this.name = name;
+    }
+
+    public void approveTeam(String teamName, Map teamsWaitlist, Map tournamentList){
+
+        Team teamToAdd = teamsWaitlist.get(teamName);
+        if(teamToAdd!=null) {
+            String desiredTournament = teamToAdd.getDesiredTournament();
+            //Map key = tournament name, value = tournament object
+            Tournament tournamentToUpdate = tournamentList.get(desiredTournament);
+            if(tournamentToUpdate!=null){
+                tournamentToUpdate.addTeam(teamToAdd);
+            }else{
+                throw new NullPointerException("Tournament does not exist");
+            }
+
+            teamsWaitlist.remove(teamName);
+        }
+        else{
+            throw new NullPointerException("Team does not exist in waiting list");
+        }
+    }
+
+}
--- src/test/java/edu/ithaca/FootballApp/registerTeamTest.java
+++ src/test/java/edu/ithaca/FootballApp/registerTeamTest.java
@@ -11,22 +11,24 @@ import static org.junit.jupiter.api.Assertions.*;
 public class registerTeamTest {
     @Test
     public static void main(String[] args) {
-        registerTeam test = new registerTeam();
         Map <String, Team> teamWaitlist = new HashMap<String, Team>();
         Map <String, Tournament> tournmentList = new HashMap<String, Tournament>();
 
         Team EvertonFC = new Team ("EvertonFC","Everton", "Desired Tournament");
         Team Testing = new Team ("Testing");
-        //TeamOwner everton = new TeamOwner("Everton");
-        //Commissioner mainC= new Commissioner ("Admin");
-        test.registerTeam(EvertonFC,teamWaitlist);
+
+        teamOwner everton = new teamOwner("Everton",EvertonFC);
+        Commissioner mainC= new Commissioner ("Admin");
+        everton.registerTeam(EvertonFC,teamWaitlist);
+        mainC.approveTeam("EvertonFC",teamWaitlist,tournmentList);
+
+        Tournament toCheck = tournmentList.get(EvertonFC.getDesiredTournament());
+        Team toFind = toCheck.getTeam(EvertonFC.getTeamName());
+
+        assertEquals(EvertonFC,toCheck);
         assertTrue(teamWaitlist.containsKey(EvertonFC.getTeamName()));
-        test.approveTeam(EvertonFC,teamWaitlist,tournmentList);
-        Tornament toCheck = tournmentList.get(EvertonFC.getDesiredTournament());
-        Team check = toCheck.getTeam(EvertonFC);
-        assertEquals(EvertonFC,check);
-        assertThrows(IllegalArgumentException.class, ()-> test.registerTeam(Testing,teamWaitlist));
-        assertThrows(NullPointerException.class, ()-> test.approveTeam(Testing.getTeamName,teamWaitlist,tournmentList m));
+        assertThrows(IllegalArgumentException.class, ()-> everton.registerTeam(Testing,teamWaitlist));
+        assertThrows(NullPointerException.class, ()-> mainC.approveTeam(Testing.getTeamName,teamWaitlist,tournmentList m));
 
 
     }
