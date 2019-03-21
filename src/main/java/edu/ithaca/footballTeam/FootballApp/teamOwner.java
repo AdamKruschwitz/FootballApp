71b4759a9afe6957671eaffe2af956cc9983ad44
esteele104 <esteele@ithaca.edu>
2019-03-21 10:25:46 -0400

    added tournament class, teamOwner, Commissioner

---
+++ src/main/java/edu/ithaca/footballTeam/FootballApp/teamOwner.java
@@ -0,0 +1,23 @@
+package edu.ithaca.footballTeam.FootballApp;
+
+import java.util.Map;
+
+public class teamOwner {
+
+    private String name;
+    private Team team;
+
+    public teamOwner(String name, Team team){
+        this.name = name;
+        this.team = team;
+    }
+
+    public void registerTeam(Team team, Map teamsWaitlist){
+        if(team.IsValid()) {
+            teamsWaitlist.put(team.teamName, team);
+        }
+        else{
+            throw new IllegalArgumentException("Team is not valid");
+        }
+    }
+}
