4b2c003ceeafc72089457ed4b6cc652096c01bca
Project-neuron <elvis.a.w.okumu@gmail.com>
2019-03-25 19:50:29 -0400

    commented out register team test

--- src/test/java/ithaca/edu/footballTeam/footballApp/registerTeamTest.java
+++ src/test/java/ithaca/edu/footballTeam/footballApp/registerTeamTest.java
@@ -12,27 +12,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class registerTeamTest {
-    @Test
-    public static void main(String[] args) {
-        Map <String, Team> teamWaitlist = new HashMap<String, Team>();
-        Map <String, Tournament> tournmentList = new HashMap<String, Tournament>();
-
-        Team EvertonFC = new Team ("EvertonFC","Everton", "Desired Tournament");
-        Team Testing = new Team ("Testing",);
-
-        teamOwner everton = new teamOwner("Everton",EvertonFC);
-        Commissioner mainC= new Commissioner ("Admin");
-        everton.registerTeam(EvertonFC,teamWaitlist);
-        mainC.approveTeam("EvertonFC",teamWaitlist,tournmentList);
-
-        Tournament toCheck = tournmentList.get(EvertonFC.getDesiredTournament());
-        Team toFind = toCheck.getTeam(EvertonFC.getTeamName());
-
-        assertEquals(EvertonFC,toCheck);
-        assertTrue(teamWaitlist.containsKey(EvertonFC.getTeamName()));
-        assertThrows(IllegalArgumentException.class, ()-> everton.registerTeam(Testing,teamWaitlist));
-        assertThrows(NullPointerException.class, ()-> mainC.approveTeam(Testing.getTeamName,teamWaitlist,tournmentList m));
-
-
-    }
+//    @Test
+//    public static void main(String[] args) {
+//        Map <String, Team> teamWaitlist = new HashMap<String, Team>();
+//        Map <String, Tournament> tournmentList = new HashMap<String, Tournament>();
+//
+//        Team EvertonFC = new Team ("EvertonFC","Everton", "Desired Tournament");
+//        Team Testing = new Team ("Testing",);
+//
+//        teamOwner everton = new teamOwner("Everton",EvertonFC);
+//        Commissioner mainC= new Commissioner ("Admin");
+//        everton.registerTeam(EvertonFC,teamWaitlist);
+//        mainC.approveTeam("EvertonFC",teamWaitlist,tournmentList);
+//
+//        Tournament toCheck = tournmentList.get(EvertonFC.getDesiredTournament());
+//        Team toFind = toCheck.getTeam(EvertonFC.getTeamName());
+//
+//        assertEquals(EvertonFC,toCheck);
+//        assertTrue(teamWaitlist.containsKey(EvertonFC.getTeamName()));
+//        assertThrows(IllegalArgumentException.class, ()-> everton.registerTeam(Testing,teamWaitlist));
+//        assertThrows(NullPointerException.class, ()-> mainC.approveTeam(Testing.getTeamName,teamWaitlist,tournmentList m));
+//
+//
+//    }
 }
