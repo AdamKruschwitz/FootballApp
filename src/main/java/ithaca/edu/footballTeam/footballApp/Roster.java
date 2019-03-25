625f59d2fe42fcb52f87cec47f846716178e6f14
Project-neuron <elvis.a.w.okumu@gmail.com>
2019-03-25 19:22:41 -0400

    added java docs to the class

--- src/main/java/ithaca/edu/footballTeam/footballApp/Roster.java
+++ src/main/java/ithaca/edu/footballTeam/footballApp/Roster.java
@@ -7,20 +7,34 @@ public class Roster {
     private boolean canPlay;
     private List<Player> players = new ArrayList<Player>();
 
-
+    /**
+     *
+     * @param canPlay hard coded boolean that determines if the roster is a playable roster
+     */
     public Roster(boolean canPlay){
         this.canPlay = canPlay;
     }
 
+    /**
+     *
+     * @return true if the roster has only eligible players and the roster count is greater than 10
+     */
     public boolean isEligible(){
         return (canPlay == true && players.size() > 10);
     }
 
+    /**
+     *
+     * @param p adds a player to the roster
+     */
     public void addPlayer(Player p){
         players.add(p);
 
     }
 
+    /**
+     * removes a player from the roster
+     */
     public void removePlayer(){
         if(players.size()>0) {
             players.remove((players.size() - 1));
@@ -30,6 +44,10 @@ public class Roster {
         }
     }
 
+    /**
+     *
+     * @return a count of all the players within the roster
+     */
     public int getPlayerCount(){
         return players.size();
     }
