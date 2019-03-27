7e40c9f038e3e4ecde85d353799cac848d69860b
esteele104 <esteele@ithaca.edu>
2019-03-27 18:21:45 -0400

    updated tests

--- src/test/java/ithaca/edu/footballTeam/footballApp/MatchTest.java
+++ src/test/java/ithaca/edu/footballTeam/footballApp/MatchTest.java
@@ -11,8 +11,17 @@ public class MatchTest {
 
     @Test
     void MatchConstructorTest() {
+        Roster activeRoster = new Roster(true);
+        List<Player> players = new ArrayList<Player>();
+        for(int i = 0; i<11; i++){
+            Player p = new Player();
+            activeRoster.addPlayer(p);
+        }
+
+        Team validTeam = new Team(activeRoster,"valid Team");
+
         List<Team> teams = new ArrayList<>();
-        teams.add(new Team());
+        teams.add(validTeam);
         Date startTime = new Date(10000000);
         Match test = new Match(teams, startTime);
         Assert.assertEquals(test.startTime, startTime);
--- src/test/java/ithaca/edu/footballTeam/footballApp/RoundTest.java
+++ src/test/java/ithaca/edu/footballTeam/footballApp/RoundTest.java
@@ -11,13 +11,25 @@ import java.util.Date;
 public class RoundTest {
     @Test
     void roundConstructorTest() {
+        Roster activeRoster = new Roster(true);
+        List<Player> players = new ArrayList<Player>();
+        for(int i = 0; i<11; i++){
+            Player p = new Player();
+            activeRoster.addPlayer(p);
+        }
+
+        Team validTeam = new Team(activeRoster,"valid Team");
+        Team validTeam2 = new Team(activeRoster,"validTeam2");
+        Team validTeam3 = new Team(activeRoster,"validTeam3");
+        Team validTeam4 = new Team(activeRoster,"validTeam4");
+
         List<Team> teams1 = new ArrayList<>();
-        teams1.add(new Team());
-        teams1.add(new Team());
+        teams1.add(validTeam);
+        teams1.add(validTeam2);
 
         List<Team> teams2 = new ArrayList<>();
-        teams2.add(new Team());
-        teams2.add(new Team());
+        teams2.add(validTeam3);
+        teams2.add(validTeam4);
 
         List<Match> matches = new ArrayList<>();
         matches.add(new Match(teams1, new Date(10000000)));
