1596708b829b173418463dc7534d4f4be8893cb7
esteele104 <esteele@ithaca.edu>
2019-03-27 19:42:29 -0400

    Updated and added javadocs to Tournament class

--- src/main/java/ithaca/edu/footballTeam/footballApp/Tournament.java
+++ src/main/java/ithaca/edu/footballTeam/footballApp/Tournament.java
@@ -9,19 +9,60 @@ public class Tournament {
 
     private Map<String, Team> teams;
     private String tournamentName;
+    private Round r1;
+    private Round r2;
+    private Round r3;
+    private Round currRound;
+    private Leaderboard leaderboard;
+
+    public Tournament(String name, Map<String, Team> teams){
+        this.tournamentName = name;
+        this.teams = teams;
+        this.r1 = new Round();
+        this.r2 = new Round();
+        this.r3 = new Round();
+        this.currRound = r1;
+        this.leaderboard = new Leaderboard();
+    }
 
     public Tournament(String name){
         this.tournamentName = name;
         this.teams = new HashMap<String,Team>();
+        this.r1 = new Round();
+        this.r2 = new Round();
+        this.r3 = new Round();
+        this.currRound = r1;
+        this.leaderboard = new Leaderboard();
 
     }
 
+    /**
+     * Advances the tournament to the next round
+     */
+    public void goToNextRound(){
+        if(currRound == r1){
+            currRound = r2;
+        }else if(currRound == r2){
+            currRound = r3;
+        }else{
+            throw new IllegalStateException("Tournament is already in final round");
+        }
+    }
+
+    /**
+     * Adds a team to the tournament
+     * @param team
+     */
     public void addTeam(Team team){
 
         teams.put(team.getTeamName(), team);
 
     }
 
+    /**
+     * Removes a team from the tournament based on team name
+     * @param teamName
+     */
     public void removeTeam(String teamName){
         Team teamToRemove = teams.get(teamName);
         if(teamToRemove!=null){
@@ -30,6 +71,11 @@ public class Tournament {
             throw new NullPointerException("This team does not exist in this tournament");
         }
     }
+
+    /**
+     * Returns the tournamentID
+     * @return tournamentID
+     */
     public String getTournamentID(){
         return tournamentName;
     }
@@ -38,5 +84,17 @@ public class Tournament {
         Team toReturn = teams.get(teamName);
         return toReturn;
     }
+
+    /**
+     * Prints out a visual of the current leaderboard for the tournament
+     */
+    public void showLeaderboard(){}
+
+    /**
+     * Updates the leaderboard for the tournament
+     */
+    public void updateLeaderboard() {}
+
+
 
 }
