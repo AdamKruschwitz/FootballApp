129e59408f4c632816aff3adf6254f5222952408
Project-neuron <elvis.a.w.okumu@gmail.com>
2019-03-31 14:43:15 -0400

    made new league and league test classes

---
+++ src/main/java/ithaca/edu/footballTeam/footballApp/League.java
@@ -0,0 +1,15 @@
+package ithaca.edu.footballTeam.footballApp;
+
+import java.util.List;
+
+public class League {
+    private String leagueName;
+    private int leagueID;
+    private List<Team> participants;
+    private List<Round> leagueRounds;
+    //private Leaderboard scoreboard;
+
+    public  League(String leagueName, int leagueID, List<Team> participants){
+
+    }
+}
---
+++ src/test/java/ithaca/edu/footballTeam/footballApp/LeagueTest.java
@@ -0,0 +1,15 @@
+package ithaca.edu.footballTeam.footballApp;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeagueTest {
+
+    @Test
+    void leagueConstructorTest(){
+
+
+    }
+}
