6fff45741823bedf2e045b9374ae79db378faf16
Project-neuron <elvis.a.w.okumu@gmail.com>
2019-04-03 16:25:51 -0400

    refactored the leaderboard

--- src/main/java/ithaca/edu/footballTeam/footballApp/Leaderboard.java
+++ src/main/java/ithaca/edu/footballTeam/footballApp/Leaderboard.java
@@ -1,60 +1,92 @@
 package ithaca.edu.footballTeam.footballApp;
 
+
 import java.util.*;
 
 
 public class Leaderboard {
     //variables
-    private HashMap leaderBoard = new HashMap<String,Integer>();
+    private HashMap<String, Integer> leaderBoard = new HashMap<String,Integer>();
+    private List<Match> playedMatches = new ArrayList<>();
 
 
     /**
-     * @Hashmaps generate a constructor
+     *
+     * @param teams takes in as an input the list of teams that will be participating in the League
+     * Creates a board that includes the team name and the team score in terms of points all teams start off with 0 points
      */
     //constructors
-    public Leaderboard(List<Team> ){
-        hs = new HashMap<String,Integer>();
-//        hs.put("teamA", 42);
-//        hs.put("teamB", 60);
-//        hs.put("teamC", 55);
-//        hs.put("teamD", 9);
+    public Leaderboard(List<Team> teams ){
+      Iterator<Team> itr = teams.iterator();
+      while (itr.hasNext()){
+          leaderBoard.put(itr.next().getTeamName(), 0);
+      }
     }
-
-    //Methods
 
     /**
-     * @return Sorted Hashmaps
+     *
+     * @param playedMatch The completed match with the statistics that you want to update the leader board with
+     * Takes a completed match and proceeds to extract the game information relevant to the leader board from it
+     *
      */
-    public String leaderboard(){
-
-        String toString = "";
-        Map<String,Integer> hsBoard = new HashMap<String,Integer>();
-//        hs.put("teamA", new Integer(42));
-//        hs.put("teamB", new Integer(60));
-//        hs.put("teamC", new Integer(55));
-//        hs.put("teamD", new Integer(9));
-
-
-
-        for (Map.Entry<String,Integer> entry : hsBoard.entrySet()){
-            System.out.println("Team: " + entry.getKey() + " Points: " + entry.getValue());
-            //toString = "Team name: " + entry.getKey() + " Points: " + entry.getValue();
+    public void updateLeaderBoard(Match playedMatch){
+        if(leaderBoard.containsKey(playedMatch.getTeam1().getTeamName())
+                && leaderBoard.containsKey(playedMatch.getTeam2().getTeamName())){
+            if(playedMatch.isTie()){
+                leaderBoard.put(playedMatch.getTeam1().getTeamName(), leaderBoard.get(playedMatch.getTeam1().getTeamName()) + 1);
+                leaderBoard.put(playedMatch.getTeam2().getTeamName(), leaderBoard.get(playedMatch.getTeam2().getTeamName()) + 1);
+            }
+            else {
+                leaderBoard.put(playedMatch.getWinner().getTeamName(), leaderBoard.get(playedMatch.getWinner().getTeamName()) + 3);
+            }
         }
-        Map<String, Integer> sortedMap = sortByValue(hsBoard); // sorts map
-        return toString;
+        else {
+            throw new IllegalArgumentException("Teams in match are not on leaderboard");
+        }
     }
 
-    private Map<String, Integer> sortByValue(Map<String, Integer> hsBoard) {
-        Map<String, Integer> sortedMap = sortByValue(hsBoard); // sorts map
-        //arraylist put hashmaps inside
-//        String[] teamsList = { "teamA","teamB","teamC","teamD" };
-        String[] sortedList = {};
-
-
-
-
-        //teamsList = hsBoard;
-        return sortedMap;
+    /**
+     *
+     * @param team team whose league score you wish to see
+     * @return the team's league score from the leader board
+     * requires the team to be in the leader board
+     */
+    public int getTeamPoints(String team){
+        if(leaderBoard.containsKey(team)){
+            return leaderBoard.get(team);
+        }
+        else{
+            throw new IllegalArgumentException("Team name not in leaderboard");
+        }
     }
+
+    /**
+     * gets the sorted leader board by the league scores returns a list of map entries with the team name
+     * @return an iterator of each team
+     */
+    public Iterator<Map.Entry<String, Integer>>  getLeaderBoard(){
+        // 1. Convert Map to List of Map
+        List<Map.Entry<String, Integer>> leagueScores =
+                new LinkedList<Map.Entry<String, Integer>>(leaderBoard.entrySet());
+
+        // 2. Sort list with Collections.sort(), provide a custom Comparator
+        //    Try switch the o1 o2 position for a different order
+        Collections.sort(leagueScores, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        return leagueScores.iterator();
+    }
+
+
+
+
+
+
+
+
 
 }
