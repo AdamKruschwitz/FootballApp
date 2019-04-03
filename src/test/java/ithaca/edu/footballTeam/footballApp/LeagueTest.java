1e63e5bbd9979a0a8fa6589e7101ab0dcdc6736c
Project-neuron <elvis.a.w.okumu@gmail.com>
2019-04-03 05:34:46 -0400

    some changes to league and league test

--- src/test/java/ithaca/edu/footballTeam/footballApp/LeagueTest.java
+++ src/test/java/ithaca/edu/footballTeam/footballApp/LeagueTest.java
@@ -11,21 +11,10 @@ public class LeagueTest {
 
     @Test
     void leagueConstructorTest(){
-
-        Roster initiallyActiveRoster = new Roster();
-        initiallyActiveRoster.fillWithValidPlayers();
-        Team validTeam = new Team(initiallyActiveRoster, "invalid Team");
-        RedCard red = new RedCard(2,"attacked ref");
-        initiallyActiveRoster.getPlayer(1).addRedCard(red);
-        List<Team> invalidTeamList = new ArrayList<Team>();
-        invalidTeamList.add(validTeam);
-        assertFalse(League.participantsValid(invalidTeamList));
-
-
         Roster activeroster = new Roster();
         activeroster.fillWithValidPlayers();
         List<Team> validTeams = new ArrayList<Team>();
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < 3; i++){
             Team newTeam = new Team(activeroster, "Valid Team");
             validTeams.add(newTeam);
         }
@@ -37,7 +26,16 @@ public class LeagueTest {
     }
 
     @Test
-    void createRoundTest(){
+    void checkIfTeamIsValidAfterCardTest(){
+        Roster initiallyActiveRoster = new Roster();
+        initiallyActiveRoster.fillWithValidPlayers();
+        Team validTeam = new Team(initiallyActiveRoster, "invalid Team");
+        List<Team> invalidTeamList = new ArrayList<Team>();
+        invalidTeamList.add(validTeam);
+        assertTrue(League.participantsValid(invalidTeamList));
+        RedCard red = new RedCard(2,"attacked ref");
+        initiallyActiveRoster.getPlayer(1).addRedCard(red);
+        assertFalse(League.participantsValid(invalidTeamList));
 
     }
 }
