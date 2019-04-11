40fb5a9352c56c8b0f24083705b30ae890037202
esteele104 <esteele@ithaca.edu>
2019-04-11 09:50:13 -0400

    Added TournamentRoundIT

--- src/test/java/ithaca/edu/footballTeam/footballApp/TournamentRoundIT.java
+++ src/test/java/ithaca/edu/footballTeam/footballApp/TournamentRoundIT.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TournamentRoundIT {
 
+    //Represent the use case of running a round in the tournament
     @Test
     public void runRoundIT(){
 
